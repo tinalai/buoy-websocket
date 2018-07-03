75b7f14443695878b4743fab38a967470995fedd
Tina Lai <tlai@d2.com>
2018-07-02 19:42:02 -0700

    set up 'Add buoy' form

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-    res.sendFile();
+    res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
